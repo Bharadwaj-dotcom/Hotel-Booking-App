29afe6d111766d8d65374db9809b29fa2e8c5de6
Bharadwaj <bharadwajbankupalli@gmail.com>
2025-06-18 11:05:18 +0530

    added .env

--- oyo_clone/settings.py
+++ oyo_clone/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from decouple import config
 import os
 from pathlib import Path
 
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b4^@az@mx5#ng8+n060n87hf&21v8#jgsj*2%4slgmf@pi2u45'
+SECRET_KEY =config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG",cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -88,10 +89,10 @@ WSGI_APPLICATION = 'oyo_clone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'oyo_clone',
-        'USER': 'root',
-        'PASSWORD': '1432',
-        'HOST':'localhost',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT':'3306',
     }
 }
